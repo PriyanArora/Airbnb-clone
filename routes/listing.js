41387c6e335696d8c5ab4770e6419435f7a6167d
Priyan Arora <Priyanarora22@gmail.com>
2026-03-19 01:36:30 -0300

    Adding passport-local-mongoose again

---
+++ routes/listing.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const Listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const { listingSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError.js");
+
+// Middleware
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//Index route
+router.get("/", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", { allListings });
+});
+
+//New route
+router.get("/new", (req, res) => {
+  res.render("./listings/new.ejs");
+});
+
+//Show route
+router.post("/", validateListing, wrapAsync(async (req, res, next) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+}));
+
+//Show route
+router.get("/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id).populate("reviews");
+  res.render("./listings/show.ejs", { listing });
+});
+
+//Edit route
+router.get("/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+});
+
+//Update route
+router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings`);
+}));
+
+//Delete route
+router.delete("/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
+
+module.exports = router;
